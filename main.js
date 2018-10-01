69c059166e2fb9e0a891cae4787ba32c1ba9554a
Tanishq <39612283+tashiCode@users.noreply.github.com>
2018-10-01 17:23:27 -0400

    LAST upload!!!!

--- main.js
+++ main.js
@@ -1,25 +1,20 @@
-// Get the modal
-var modal = document.getElementById('myModal');
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal 
-btn.onclick = function() {
-    modal.style.display = "block";
+function failAlert() {
+    alert("An error occurred. Please try again.");
+    setTimeout(function() {window.location.href = 'index.html'; } ,1000);
 }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+function move() {
+    var elem = document.getElementById("myBar"); 
+    var width = 10;
+    var id = setInterval(frame, 100);
+    function frame() {
+        if (width >= 99) {
+            clearInterval(id);
+            setTimeout(failAlert,2500);
+        } else {
+            width++; 
+            elem.style.width = width + '%'; 
+            elem.innerHTML = width * 1 + '%';
+        }
     }
 }
